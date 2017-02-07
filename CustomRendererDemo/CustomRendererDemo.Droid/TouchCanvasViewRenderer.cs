f462ec6b0c1bc3f5c4d4e47b4d7b8e49f11fb8c2
Matthew Leibowitz <mattleibow@live.com>
2017-02-07 17:58:10 +0400

    Added the Android renderer

---
+++ CustomRendererDemo/CustomRendererDemo.Droid/TouchCanvasViewRenderer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Android.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+
+[assembly: ExportRenderer(typeof(CustomRendererDemo.TouchCanvasView), typeof(CustomRendererDemo.Droid.TouchCanvasViewRenderer))]
+
+namespace CustomRendererDemo.Droid
+{
+	public class TouchCanvasViewRenderer : SKCanvasViewRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<SKCanvasView> e)
+		{
+			// clean up old native control
+			if (Control != null)
+			{
+				Control.Touch -= OnTouch;
+			}
+
+			// do clean up old element
+			if (Element != null)
+			{
+				var oldTouchCanvas = (TouchCanvasView)Element;
+				var oldTouchController = (ITouchCanvasViewController)Element;
+
+				// ...
+			}
+
+			base.OnElementChanged(e);
+
+			// set up new native control
+			if (Control != null)
+			{
+				Control.Touch += OnTouch;
+			}
+
+			// set up new element
+			if (e.NewElement != null)
+			{
+				var newTouchCanvas = (TouchCanvasView)Element;
+				var newTouchController = (ITouchCanvasViewController)Element;
+
+				// ...
+			}
+		}
+
+		private void OnTouch(object sender, TouchEventArgs e)
+		{
+			var touchController = Element as ITouchCanvasViewController;
+			if (touchController != null)
+			{
+				var wasTap = e.Event.ActionMasked == MotionEventActions.Up || e.Event.ActionMasked == MotionEventActions.Cancel;
+				if (wasTap)
+				{
+					var scale = Control.Resources.DisplayMetrics.Density;
+					touchController.OnTouch(new SKPoint(e.Event.GetX() / scale, e.Event.GetY() / scale));
+				}
+			}
+		}
+	}
+}
